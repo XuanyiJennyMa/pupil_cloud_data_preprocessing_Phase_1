a0e9566a662f3a0017dc3af510b69ba301856312
XuanyiJennyMa <158461054+XuanyiJennyMa@users.noreply.github.com>
2024-02-15 17:14:28 +0000

    Update Step0_0_AOI_extraction.py

--- Step0_0_AOI_extraction.py
+++ Step0_0_AOI_extraction.py
@@ -13,15 +13,14 @@ import numpy as np
 import pandas as pd
 import os
 
-# Set the directory of the folder containing all reference pictures.
-path_to_reference_image = ("./Reference_pic") 
+# Set the directory of the folder containing reference pictures.
+path_to_reference_image_single_aoi = ("./Reference_pic/Single_aoi") 
+path_to_reference_image_complex_aoi = ("./Reference_pic/Complex_aoi")
 
 # This function is for reading the reference picture name. 
 # Original code can be found in Pupil Labs' script as indicated above.
 # Change the file extension within the quotation mark if your picures are in different format.
 extract_name = lambda name: name.split(".jpeg")[0] 
-
-
 
 # This function is to extract the information of each referencec picture and then save it in a dictionary with the reference picure name as the key. 
 def extract_ref_pics(path):
@@ -35,8 +34,9 @@ def extract_ref_pics(path):
         ref_pic_dict[extract_name(pic)] = reference_image 
     return ref_pic_dict
 
-# Run the above function with all the reference pictures in the set-up folder
-reference_picture = extract_ref_pics(path_to_reference_image)     
+# Run the above function with the reference pictures in the set-up folders
+reference_picture_single_aoi = extract_ref_pics(path_to_reference_image_single_aoi) 
+reference_picture_complex_aoi = extract_ref_pics(path_to_reference_image_complex_aoi)
 
 # Some picures 
 # this function is for extracting the AOIs once for each picture
@@ -54,6 +54,8 @@ def extract_aoi_info_diff_pic(ref_pic_list):
         aoi_df = pd.DataFrame(aois)
         filename = pic+"_AOIs.csv"
         aoi_df.to_csv(filename, index=False)
+
+
 
 def extract_aoi_info_same_pic(ref_pic, aoi_versions):
     for version in range(aoi_versions):
