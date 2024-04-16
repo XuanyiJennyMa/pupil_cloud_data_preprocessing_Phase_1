de49cc1dd3b665e76609da25166de4c32cf756bb
Xuanyi Ma <xuanyim@psy.gla.ac.uk>
2024-04-16 10:56:40 +0100

    modified version for new datasetes

--- Step3_Label_pupil_diameter_with_aoi_section.py
+++ Step3_Label_pupil_diameter_with_aoi_section.py
@@ -1,59 +1,17 @@
-
-# Import libraries
-
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 import csv
+import glob
 
-def extract_fixation_info(pic, multi_version_check, overlapped_aois, version):
-    if multi_version_check == True:
-        aoi_version = version
-        aoi_column = 'AOI_' + str(version)
-        if overlapped_aois == True:
-            csv_file = 'final_labelled_fixations_diff_version.csv'
-        else:
-            csv_file = 'labelled_fixations_diff_version.csv'
-    else:
-        aoi_column = 'AOI' 
-        csv_file = 'labelled_fixations.csv'
-   
-    filepath = "./" + pic
-    aoi_labelled_fixations = pd.read_csv(f"{filepath}/{csv_file}")
-    
-    number_of_fixations = np.unique(aoi_labelled_fixations[aoi_column])
-    number_of_participants = np.unique(aoi_labelled_fixations['Participant'])
-    
-    fixation_participant_dict = {}
-    for participant in range(max(number_of_participants)):
-        for idx in range(len(number_of_fixations)-1):
-            selected_participant = aoi_labelled_fixations[aoi_labelled_fixations['Participant'] == (participant + 1)]
-            selected_fixation = selected_participant[selected_participant[aoi_column] == idx]
-            list_of_index = selected_fixation.index.to_list()
-            
-            list_fixation = []
-            list_of_list_fixation = []
-            for index_no in list_of_index:
-                if list_of_index.index(index_no) != len(list_of_index)-1:
-                    if list_of_index[list_of_index.index(index_no)+1] - index_no == 1:
-                        list_fixation.append(index_no)
-                    else:
-                        list_fixation.append(index_no)
-                        list_of_list_fixation.append(list_fixation)
-                        list_fixation=[]
-                else:
-                    list_fixation.append(index_no)
-            list_of_list_fixation.append(list_fixation)
-            key_name = 'participant_' + str(participant +1) + '_fixation_' + str(idx)
-            fixation_participant_dict[key_name] = list_of_list_fixation
-    
-    csv_filename = pic + '_' + aoi_column + '_fixation_dict.csv'
-    with open(csv_filename, 'w') as f:  
-        w = csv.DictWriter(f, fixation_participant_dict.keys())
-        w.writeheader()
-        w.writerow(fixation_participant_dict)            
-    return fixation_participant_dict
+section_info_df = pd.read_csv("Timeseries Data/sections.csv")
+wearer_name = [participant for index, participant in enumerate(section_info_df['wearer name']) if 'scanning' not in participant]
 
+for wearer in wearer_name:
+    data_folder = glob.glob('./Timeseries Data/'+ "*" + wearer)
+    pupil_df = pd.read_csv(f"{data_folder[0]}/3d_eye_states.csv")
+    pupil_df['aoi_column'] = None
+    pupil_df.to_csv(f"{data_folder[0]}/labelled_pupil.csv", index=False)
 
 def label_pupil_with_aoi(pic, multi_version_check, overlapped_aois, version):
     if multi_version_check == True:
@@ -67,23 +25,27 @@ def label_pupil_with_aoi(pic, multi_version_check, overlapped_aois, version):
         aoi_column = 'AOI' 
         csv_file = 'labelled_fixations.csv'
    
-    labelled_fixations_filepath = "./" + pic
-    aoi_labelled_fixations = pd.read_csv(f"{labelled_fixations_filepath}/{csv_file}")
-    
-    number_of_fixations = np.unique(aoi_labelled_fixations[aoi_column])
-    number_of_participants = np.unique(aoi_labelled_fixations['Participant'])
-    
+    # Import the fixation data
+    # If you have changed the folder name to be the same as the picture name
+    # data_folder = "./" + pic
+    # If you keep the folder name as how it was downloaded from Pupil Cloud, comment the line above and uncomment the two lines below:
+    foldername = glob.glob('*' + pic + '_csv')
+    data_folder = "./" + foldername[0]
+    aoi_labelled_fixations = pd.read_csv(f"{data_folder}/{csv_file}")
     
     fixation_participant_dict = {}
-    for participant in range(max(number_of_participants)):
-        pupil_filepath = "./Timeseries_Data/" + pic + "_" + str(participant+1).zfill(3)
-        pupil_diameter = pd.read_csv(f"{pupil_filepath}/3d_eye_states.csv")
+    for wearer in wearer_name:
+        pupil_data_folder = glob.glob('./Timeseries Data/'+ "*" + wearer)
+        pupil_diameter = pd.read_csv(f"{pupil_data_folder[0]}/labelled_pupil.csv")
 
         start_time_list = []
         end_time_list = []
-        for idx in range(len(number_of_fixations)-1):
-            selected_participant = aoi_labelled_fixations[aoi_labelled_fixations['Participant'] == (participant + 1)]
-            selected_fixation = selected_participant[selected_participant[aoi_column] == idx]
+        selected_participant = aoi_labelled_fixations[aoi_labelled_fixations['Participant'] == int(wearer)]
+        valid_fixations = [fixation for index, fixation in enumerate(selected_participant[aoi_column]) if not pd.isna(fixation)]
+        fixation_list = np.unique(valid_fixations)
+        
+        for fix in fixation_list:
+            selected_fixation = selected_participant[selected_participant[aoi_column] == fix]
             list_of_index = selected_fixation.index.to_list()
             
             list_fixation = []
@@ -101,7 +63,7 @@ def label_pupil_with_aoi(pic, multi_version_check, overlapped_aois, version):
                     
                     
             list_of_list_fixation.append(list_fixation)
-            key_name = 'participant_' + str(participant +1) + '_fixation_' + str(idx)
+            key_name = 'participant_' + wearer + '_fixation_' + str(int(fix))
             fixation_participant_dict[key_name] = list_of_list_fixation
             
             start_time = []
@@ -112,19 +74,15 @@ def label_pupil_with_aoi(pic, multi_version_check, overlapped_aois, version):
             start_time_list.append(start_time)  
             end_time_list.append(end_time)  
             
-        pupil_diameter[aoi_column] = None
-        
 
-        for i in range(0, len(start_time_list)):
+        for i, fix in enumerate(fixation_list):
             for j in range(0, len(start_time_list[i])):
-                print(i, j)
                 subset = pupil_diameter[(pupil_diameter['timestamp [ns]'] >= start_time_list[i][j]) & (pupil_diameter['timestamp [ns]'] <= end_time_list[i][j])]
-                #i -- AOI, j -- fixation group
+                #fix -- AOI, j -- fixation group
                 index_list = subset.index
-                pupil_diameter.loc[index_list, aoi_column] = str(i)+str(j)
+                pupil_diameter.loc[index_list, 'aoi_column'] = str(int(fix))+ '_' + str(j)
         
-        pupil_filename = pic + '_labelled_pupil_' + str(participant+1).zfill(3) + '.csv'
-        pupil_diameter.to_csv(pupil_filename, index=False)
+        pupil_diameter.to_csv(f"{pupil_data_folder[0]}/labelled_pupil.csv", index=False)
                 
     
     csv_filename = pic + '_' + aoi_column + '_fixation_dict.csv'
@@ -134,10 +92,4 @@ def label_pupil_with_aoi(pic, multi_version_check, overlapped_aois, version):
         w.writerow(fixation_participant_dict)            
     return fixation_participant_dict
 
-pupil_filepath = "./Timeseries_Data/" + 'Manuscript_1' + "_" + str(1).zfill(3)
-pupil_diameter = pd.read_csv(f"{pupil_filepath}/3d_eye_states.csv")
-print(pupil_diameter)
-pupil_diameter['test'] = None
-pupil_filename = 'Manuscript_1' + '_labelled_pupil_test_' + str(1).zfill(3) + '.csv'
-        
-pupil_diameter.to_csv(pupil_filename, index=False)
+
